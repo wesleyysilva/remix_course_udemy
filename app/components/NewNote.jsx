30cd3fc4173144989878c7620d0dc4cf7cd07835
Wesley Silva <wesleyysilva@yahoo.com.br>
2022-12-13 23:46:58 -0300

    update NewNote component

--- app/components/NewNote.jsx
+++ app/components/NewNote.jsx
@@ -1,8 +1,14 @@
+import { Form, useTransition as useNavigation } from '@remix-run/react';
 import styles from './NewNote.css';
 
 function NewNote() {
+
+	const navigation = useNavigation();
+
+	const isSubmitting = navigation.state === 'submitting';
+
 	return (
-		<form method="post" id="note-form">
+		<Form method="post" id="note-form">
 			<p>
 				<label htmlFor="title">Title</label>
 				<input type="text" id="title" name="title" required />
@@ -12,9 +18,11 @@ function NewNote() {
 				<textarea id="content" name="content" rows="5" required />
 			</p>
 			<div className="form-actions">
-				<button>Add Note</button>
+				<button disabled={isSubmitting}>
+					{isSubmitting ? 'Adding...' : 'Add Note'}
+				</button>
 			</div>
-		</form>
+		</Form>
 	);
 }
 
